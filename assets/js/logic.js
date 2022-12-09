0785e67fd0f7201a7463d8876270fe59240b6136
Pavel Naumovich <pavelnaumovic@gmail.com>
2022-12-09 20:55:34 +0000

    questions done

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,13 @@
 import { questionList } from "./questions.js";
 
-const questionWrapper = document.getElementById("questions");
 const questionH2 = document.getElementById("question-title");
 const choisesDiv = document.getElementById("choices");
+const feedbackDiv = document.getElementById("feedback");
 const startQuizBtn = document.getElementById("start");
+const finalScore = document.getElementById("final-score");
+const submitBtn = document.getElementById("submit");
+
+let timerID = undefined;
 let remainingTime = 75;
 let currentQuestion = 0;
 
@@ -15,7 +19,7 @@ function showDiv(divID) {
 
 function hideDiv(divID) {
   let targetDiv = document.getElementById(divID);
-  targetDiv.setAttribute("class", "hide");
+  targetDiv.setAttribute("class", "hide feedback");
 }
 
 function renderQuestion(question) {
@@ -30,31 +34,84 @@ function renderQuestion(question) {
   }
   choisesDiv.appendChild(ol);
   showDiv("questions");
+  currentQuestion++;
 }
 
-function checkAnswer(e) {
-  const userChoise = e.target.innerText;
-  const correctAnswer = questionList[currentQuestion].answer;
-  if (userChoise === correctAnswer) {
-    correctAnswerRoutine();
+function userSelectionHandler(e) {
+  if (currentQuestion < questionList.length) {
+    const userChoise = e.target.innerText;
+    const correctAnswer = questionList[currentQuestion - 1].answer;
+    if (userChoise === correctAnswer) {
+      correctAnswerRoutine();
+    } else {
+      wrongAnswerRoutine();
+    }
+    questionH2.innerHTML = ""; //Clear previous question.
+    choisesDiv.innerHTML = ""; //Clear previous choises.
+    renderQuestion(questionList[currentQuestion]);
+  } else {
+    finalScore.textContent = remainingTime;
+    showDiv("feedback");
+    hideDiv("questions");
+    showDiv("end-screen");
+    document;
+    setTimeout(function () {
+      hideDiv("feedback");
+    }, 2000);
+    clearInterval(timerID);
   }
 }
 
 function correctAnswerRoutine() {
   showDiv("feedback");
-  const audio = new Audio("../sfx/correct.wav");
+  feedbackDiv.textContent = "Correct!";
+  const audio = new Audio("/starter/assets/sfx/correct.wav");
   audio.play();
+  setTimeout(function () {
+    hideDiv("feedback");
+  }, 2000);
 }
 
-function wrongAnswer() {}
+function wrongAnswerRoutine() {
+  remainingTime -= 15;
+  showDiv("feedback");
+  feedbackDiv.textContent = "Wrong!";
+  const audio = new Audio("/starter/assets/sfx/incorrect.wav");
+  audio.play();
+  setTimeout(function () {
+    hideDiv("feedback");
+  }, 2000);
+}
 
 function startTimer() {
-  setInterval(function () {
+  timerID = setInterval(function () {
     if (remainingTime > 0) {
       remainingTime--;
       document.getElementById("time").textContent = remainingTime;
     }
   }, 1000);
+}
+
+function submitBtnEventHandler() {
+  const initials = document.getElementById("initials").value;
+  syncWithLocalStorage(initials, remainingTime);
+}
+
+function syncWithLocalStorage(initials, userScore) {
+  if (!localStorage.getItem("highScores")) {
+    let highScores = {
+      [`${initials}`]: userScore,
+    };
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+  } else {
+    let highScores = JSON.parse(localStorage.getItem("highScores"));
+    highScores[initials] = userScore;
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+  }
+}
+
+function loadFromLocalStorage() {
+  
 }
 
 function startQuiz() {
@@ -64,4 +121,5 @@ function startQuiz() {
 }
 
 startQuizBtn.addEventListener("click", startQuiz);
-choisesDiv.addEventListener("click", checkAnswer);
+choisesDiv.addEventListener("click", userSelectionHandler);
+submitBtn.addEventListener("click", submitBtnEventHandler);
